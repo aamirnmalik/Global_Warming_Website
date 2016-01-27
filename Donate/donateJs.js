6aff11ec5f29083e4d60dfcce82e6f9f0014fa31
Hanying Dong <donghanying1@gmail.com>
2016-01-27 12:55:21 -0500

    fixed donate

--- Donate/donateJs.js
+++ Donate/donateJs.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 
 	$("#buttonOne").click(function(e){
+		$("#firstP").hide();
 		$("#formOne").hide();
+		$(".thankyou").show();
 		$("#formTwo").show();
+		$("#buttonOne").hide();
+		e.preventDefault();
+	});
+
+	$("#buttonTwo").click(function(e){
+
+		$("form").hide();
+		$("#firstP").hide();
+		$("#formOne").hide();
+		$("#buttonOne").hide();
+		$(".thankyou").hide();
+		$("#last").show();
 		e.preventDefault();
 	});
 })
