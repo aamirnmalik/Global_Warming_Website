0f79d078c4059e01fcf44e9c18bf36db37802899
Yakov Pinhasov <yakovpinhasov@gmail.com>
2016-01-27 16:08:42 -0500

    added new question form

--- info/finalProject.js
+++ info/finalProject.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
     $("#video")[0].src += "&autoplay=1";
     ev.preventDefault();
   });
+  $("#buttonOne").click(function(e){
+		$(".iform").hide();
+		$(".thankyou ").show();
+		$("#buttonOne").hide();
+		$("#video").show();
+		$("#buttonTwo").show();
+		e.preventDefault();
+	});
+  $("#buttonTwo").click(function(e){
+		$(".thankyou ").hide();
+		$("#buttonOne").hide();
+		$("#video").hide();
+		$(".iform").show();
+		$("#buttonTwo").hide();
+		$("#buttonOne").show();
+		e.preventDefault();
+	});
 });
