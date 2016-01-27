a68a71af89a448fb565c9661b8dbb2ab50a69c5a
Hanying Dong <donghanying1@gmail.com>
2016-01-27 15:08:16 -0500

    add video and fixed some error on donate page

--- Donate/donateJs.js
+++ Donate/donateJs.js
@@ -11,20 +11,33 @@ $(document).ready(function(){
 
 	$("#buttonTwo").click(function(e){
 
-		$("form").hide();
+		// $("form").hide();
 		$("#firstP").hide();
 		$("#formOne").hide();
 		$("#buttonOne").hide();
 		$(".thankyou").hide();
+		$("#formTwo").hide();
 		$("#last").show();
+		$("#buttonThree").show();
 		e.preventDefault();
 	});
 
-	$("#last").click(function(){
+	$("#buttonThree").click(function(e){
+		$("#buttonThree").hide();
+		$(".video").show();
+		// $("#last").hide();
+		// $("#firstP").show();
+		// $("#formOne").show();
+		// $("#buttonOne").show();
+		e.preventDefault();
+	})
+	$("#last").click(function(e){
+		
+		$(".video").hide();
 		$("#last").hide();
 		$("#firstP").show();
 		$("#formOne").show();
 		$("#buttonOne").show();
-		
+		e.preventDefault();
 	})
 })
